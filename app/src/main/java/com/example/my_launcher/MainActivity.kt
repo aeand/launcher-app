6b89dc582ba38a7141649cb93b6b375c361d517c
aeand <60613804+aeand@users.noreply.github.com>
2024-10-25 14:06:52 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/my_launcher/MainActivity.kt
+++ app/src/main/java/com/example/my_launcher/MainActivity.kt
@@ -80,7 +80,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
