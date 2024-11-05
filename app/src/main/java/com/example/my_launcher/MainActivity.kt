200f543c79fcf079251c19d77eda9de11bbf9f8e
aeand <60613804+aeand@users.noreply.github.com>
2024-11-05 17:00:11 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/my_launcher/MainActivity.kt
+++ app/src/main/java/com/example/my_launcher/MainActivity.kt
@@ -83,6 +83,7 @@ The hitbox for button J broke when the app was alone in J (could be the letters 
 /* TODO Notes
 - add override dialog that will handle saving file with same name as other file
 - press and hold to delete/move notes in directory
+- add folder button
 */
 
 /* Intent list that would be useful
