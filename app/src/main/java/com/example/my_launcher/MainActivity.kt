0f605d889612144415c208afae444d6ed42cfe8f
aeand <aeand2@gmail.com>
2025-04-12 22:21:06 +0200

    updated todos

--- app/src/main/java/com/example/my_launcher/MainActivity.kt
+++ app/src/main/java/com/example/my_launcher/MainActivity.kt
@@ -41,6 +41,9 @@ import java.util.concurrent.TimeUnit
 
 /* TODO
 save hidden apps to file
+add a log file for logging all restarts
+
+make eye hitbox a lot bigger. not the icon tho
 
 when tapping eye open a checkbox for every app to select what apps to hide
 and replace long press on app to open app settings
@@ -48,13 +51,37 @@ and remove back to top arrow
 
 clean up unused icons, fonts, and similar
 
-fix delay on startup by doing intents on non-blocking main thread
+remove edge to edge. Was it for the trasparent colors?
+
+when going to settings i should go to top of app list
+
+add a safety check that apps exist before opening them or settings so app dont crash
+
+make it rotatable
+
+load apps on sidethread and fill after each app loaded to make it faster??
+
+disable battery saving for app?
+
+are there any settings that are worth a settings page?
+
+home works for pie launcher :eyes: checkout their code!!!
+
+make padding on sides of app list part of scrolling app list instead of scrolling pager.
+cause i sometimes scroll at the very edge of my phone
+
+sometimes when pressing back the fade takes a loong time to clear
+
+load apps on separate thread to make it faster and not freeze
+
 check out recompositions. reduce them as much as possible
+
 refresh app list after install
+
 fix app select background (currently grey)
+
 app select background shows when opening app select, locking, and then unlocking phone
 
-//test without alphabet list (unused feature)
 improve the pager to swipe faster
 */
 
